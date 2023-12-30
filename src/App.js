e58ec1c4666f1bd16276503f257b8a6920aa6b07
RahimRind <abdulrahimrind022@gmail.com>
2023-12-30 15:37:02 +0500

    quiz app half work done

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const [questions, setQuestions] = useState([])
 const [userAnswer, setuserAnsewers] = useState()
 const [currentIndex, setCurrentIndex] = useState(0)
 const [score , setScore] = useState (0)
-
+const [isOptionSelected, setIsOptionSelected] = useState(false);
 
  useEffect(function(){
 renderApi()
@@ -22,7 +22,7 @@ function renderApi(){
      item.options = shuffle(item.options);
     })
     setQuestions(res)
-    console.log(res);
+    // console.log(res);
   })
 }
 function shuffle(array) {
@@ -42,46 +42,38 @@ function shuffle(array) {
 
   return array;
 }
+ 
 
-
-
-
-  if(!questions.length){
+if(!questions.length){
     return <h1>Loading...</h1>
   }
 
 
 
-function nextq(){
+function nextbtn(){
   // setCurrentIndex(currentIndex + 1)
   if(userAnswer === questions[currentIndex].correctAnswer){
    setScore(score + 10)
   }
   setCurrentIndex(currentIndex + 1)
-  console.log(questions[currentIndex].correctAnswer);
-
+  // console.log(questions[currentIndex].correctAnswer);
+  setIsOptionSelected(false) 
 }
-function Restartq(){
+function Restartbtn(){
   setCurrentIndex(0)
+  setIsOptionSelected(false)
+  setScore(0)
 }
 
-function changeText (event){
-  const textValue = event.target.value
-  console.log(textValue);
-setuserAnsewers(textValue)
-
-
-
-
+function changeText(event) {
+  const textValue = event.target.value;
+  // console.log(textValue);
+  setuserAnsewers(textValue);
+  setIsOptionSelected(true); // Fix the typo here
 }
 
 
-
-
-
-
-
-const currentQuetions = questions[currentIndex]
+const currentQuestions = questions[currentIndex]
 
 const quizEnded = currentIndex === questions.length
 
@@ -92,56 +84,45 @@ const quizEnded = currentIndex === questions.length
      
      
       {!quizEnded ?  <div className=' q'>
-       <h3 className='border 3px bg-black p-4 2px rounded-lg'  >Q.{currentIndex + 1} {currentQuetions.question.text}</h3>
+      
+      
+  
+       <h3 className='border 3px bg-black p-4 2px rounded-lg'  >Q.{currentIndex + 1} {currentQuestions.question.text}</h3>
 
-        {currentQuetions.options.map(function(item){
-          return <dir>
+        {currentQuestions.options.map(function(item,index){
+          return <div key={index}>
              <input
               name='answer'
               type="radio" 
               value={item} 
               onChange={changeText}/>
              {item}
-          </dir>
+          </div>
         })}
 
-        <h3>
-          {score}
-        </h3>
-<button onClick={nextq}>Next</button>
+        <button onClick={nextbtn} disabled={!isOptionSelected}>
+  Next
+</button>
 
 </div>
 
 :
 
 <div className='r'>
- <h2>Result</h2>
+ {/* <h2>Result</h2> */}
 
+ <h1>Final Results {score} out of {questions.length} </h1>
+          <h2>
+             Correct  Is Answer =   {(score / questions.length) }
+          </h2>
+  <h3>Total Result is   {score}% 
+  </h3>
+  
 
-
-  <button onClick={Restartq}>Restart</button>
+  <button onClick={Restartbtn}>Restart</button>
 
 </div>
-
 }
-
-
-      {/* <ol className=' m-2'>
-        <li  className='cursor-pointer bg-red-800 m-2 border 2px rounded-lg p-2 '>{questions[currentIndex].correctAnswer} </li>
-        <li className=' cursor-pointer bg-red-800 m-2 border 2px rounded-lg p-2 ' >{questions[currentIndex].incorrectAnswers[0]} </li>
-        <li className='cursor-pointer bg-red-800 m-2 border 2px rounded-lg p-2 ' >{questions[currentIndex].incorrectAnswers[1]}</li>
-        <li className='cursor-pointer bg-red-800 m-2 border 2px rounded-lg p-2 '>{questions[currentIndex].incorrectAnswers[2]}</li>
-      </ol> */}
-
-{/* 
-      <h4 className='font-normal text-base'>{currentIndex +  1} To 10 Question</h4>
-
-      
-      {currentIndex === questions.length -1 ?   
-       <button className='border 2px mt-4 rounded-xl bg-red-700 px-5 py-2' onClick={RestartIndex}>Restart</button>
-       :
-       <button className='border 2px mt-4 rounded-xl bg-blue-700 px-5 py-2' onClick={nextIndex}>Next</button>
-      } */}
            
       </header>
     </div>
