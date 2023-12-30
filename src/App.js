6391a874e8bc97c3e7fa71c5bf0f799e94329161
RahimRind <abdulrahimrind022@gmail.com>
2023-12-31 00:36:54 +0500

    quiz app almst done

--- src/App.js
+++ src/App.js
@@ -8,10 +8,31 @@ const [userAnswer, setuserAnsewers] = useState()
 const [currentIndex, setCurrentIndex] = useState(0)
 const [score , setScore] = useState (0)
 const [isOptionSelected, setIsOptionSelected] = useState(false);
+const [timer, setTimer] = useState(600); // Set the initial timer value in seconds
+const [quizEnd, setQuizEnd] = useState(false);
 
- useEffect(function(){
-renderApi()
- },[]) 
+
+useEffect(() => {
+  if (!quizEnd) {
+    const timerInterval = setInterval(() => {
+      setTimer((prevTimer) => {
+        if (prevTimer === 0) {
+          clearInterval(timerInterval);
+          setQuizEnd(true);
+        }
+        return prevTimer - 1;
+      });
+    }, 1000);
+    return () => clearInterval(timerInterval);
+  }
+}, [quizEnd]);
+
+useEffect(() => {
+  renderApi();
+}, []);
+// useEffect(function(){
+// renderApi()
+//  },[]) 
 
 function renderApi(){
   fetch('https://the-trivia-api.com/v2/questions')
@@ -54,6 +75,7 @@ function nextbtn(){
   // setCurrentIndex(currentIndex + 1)
   if(userAnswer === questions[currentIndex].correctAnswer){
    setScore(score + 10)
+   setQuizEnd(true)
   }
   setCurrentIndex(currentIndex + 1)
   // console.log(questions[currentIndex].correctAnswer);
@@ -63,7 +85,13 @@ function Restartbtn(){
   setCurrentIndex(0)
   setIsOptionSelected(false)
   setScore(0)
+  setTimer(600)
+  setQuizEnd(false)
 }
+
+
+const minutes = Math.floor(timer / 60);
+const seconds = timer % 60;
 
 function changeText(event) {
   const textValue = event.target.value;
@@ -79,13 +107,14 @@ const quizEnded = currentIndex === questions.length
 
   return (
     <div className="App "> 
-      <header className="App-header bg-black   ">
+      <header className="App-header">
+        <div className='border 4px p-5 bg-gray-800 rounded-2xl shadow-2xl'>
       <h1 className='font-bold text-4xl m-10'><i>Quiz App</i></h1> 
      
      
       {!quizEnded ?  <div className=' q'>
       
-      
+        
   
        <h3 className='border 3px bg-black p-4 2px rounded-lg'  >Q.{currentIndex + 1} {currentQuestions.question.text}</h3>
 
@@ -99,8 +128,13 @@ const quizEnded = currentIndex === questions.length
              {item}
           </div>
         })}
+        <div className=' rounded-2xl'>Yuor Score Is {score}</div>
 
-        <button onClick={nextbtn} disabled={!isOptionSelected}>
+        <div className="text-red-500">
+                Time remaining: {minutes} minutes {seconds} seconds
+              </div>
+
+        <button onClick={nextbtn} className='border 5px rounded-xl bg-blue-800 px-4' disabled={!isOptionSelected}>
   Next
 </button>
 
@@ -123,7 +157,7 @@ const quizEnded = currentIndex === questions.length
 
 </div>
 }
-           
+</div>    
       </header>
     </div>
   );
